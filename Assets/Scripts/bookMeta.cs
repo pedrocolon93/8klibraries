99927352cfe5375f93d135dd675c1949a75ae11a
Pedro <pe25171@mit.edu>
2018-06-29 15:22:06 -0400

    Initial import of project. Using editor 2017

---
+++ Assets/Scripts/bookMeta.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bookMeta : MonoBehaviour {
+    //for keeping track of the basic metadata of a single book.
+    static public string title;
+
+	// Use this for initialization
+	void Start () {
+        title = "Siddhartha.txt";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/selectBook.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selectBook : MonoBehaviour {
+	private GameObject heldObject, book;
+	Collider held;
+
+	// Use this for initialization
+	void Start () {
+		//held = null;//you could hack this by setting it to an empty game object if you're not allowed to do this, and them checking if what you're holding is that empty object
+		//for now, this is hardcoded
+		heldObject = GameObject.Find("closedBook");
+		held = heldObject.GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space")){
+			Debug.Log("You pressed the space bar!");
+			if(!held.Equals(null)){//should you be using == here instead???
+				Destroy(heldObject);//does this set things to null or do you now have a weird broken ref?
+				book = Instantiate(Resources.Load("Book")) as GameObject;
+			}
+		}
+		
+	}
+
+	// //are you holding something right now?
+	// //you can't test this until you know how hands work.
+	// void OnTriggerEnter(Collider other){
+	// 	held = other.gameObject;
+	// 	Debug.log("You're holding a thing!\n" + held);
+	// }
+
+	// void OnTriggerExit(Collider other){
+	// 	held = null;
+	// }
+
+
+}
